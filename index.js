0f3c5e2ee62c87b6237e4b53d18eeafa271c5d70
Yaungzawae <103674409+Yaungzawae@users.noreply.github.com>
2022-12-22 02:54:11 +0630

    Delete index.js

--- index.js
+++
@@ -1,5 +0,0 @@
-// setTimeout(() => {
-//   var userName = prompt("What's your name?");
-//   document.querySelector(".userName").innerText = userName;
-//   document.querySelector(".userName").style.visibility = "visible";
-// }, 500);
